29847d1a16720ae3294ca95979f19e86466478ee
jrydel <rydel@gloffer.com>
2020-04-30 11:01:21 +0200

    #U - init

--- src/main/java/cz/jr/trailtour/backend/controler/StageController.java
+++ src/main/java/cz/jr/trailtour/backend/controler/StageController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
@@ -31,22 +33,22 @@ public class StageController {
     }
 
     @CrossOrigin
-    @GetMapping(value = "/getSegments", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Stage>> getSegments() {
+    @GetMapping(value = "/getStages", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Stage>> getStages() {
         return new ResponseEntity<>(stageService.getAll(), HttpStatus.OK);
     }
 
-    @CrossOrigin
-    @PostMapping(path = "/saveSegment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> saveSegment(@RequestBody Stage stage) {
-        stageService.save(stage);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @CrossOrigin
-    @PostMapping(path = "/deleteSegment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> deleteSegment(@RequestBody Stage stage) {
-        stageService.delete(stage);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+//    @CrossOrigin
+//    @PostMapping(path = "/saveSegment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Void> saveSegment(@RequestBody Stage stage) {
+//        stageService.save(stage);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @CrossOrigin
+//    @PostMapping(path = "/deleteSegment", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Void> deleteSegment(@RequestBody Stage stage) {
+//        stageService.delete(stage);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
 }
