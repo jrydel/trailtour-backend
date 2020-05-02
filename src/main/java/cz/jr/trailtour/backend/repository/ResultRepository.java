4396adae97f00cc22a28d3125a636bc9f6b1d967
jrydel <rydel@gloffer.com>
2020-05-02 16:58:13 +0200

    #U - feed api

--- src/main/java/cz/jr/trailtour/backend/repository/ResultRepository.java
+++ src/main/java/cz/jr/trailtour/backend/repository/ResultRepository.java
@@ -24,10 +24,10 @@ public class ResultRepository extends MysqlRepository {
     }
 
     public List<Feed> getFeed(int limit) throws SQLException {
-        return selectList("SELECT a.activity_id, a.updated, a.time, a.position, b.id, b.name, c.id, c.name, c.number FROM trailtour.result a JOIN trailtour.athlete b ON a.athlete_id = b.id JOIN trailtour.stage c ON c.id = a.stage_id ORDER BY a.updated DESC LIMIT ?", new Object[]{limit}, rs -> {
+        return selectList("SELECT a.activity_id, a.created, a.time, a.position, b.id, b.name, c.id, c.name, c.number FROM trailtour.result a JOIN trailtour.athlete b ON a.athlete_id = b.id JOIN trailtour.stage c ON c.id = a.stage_id ORDER BY a.updated DESC LIMIT ?", new Object[]{limit}, rs -> {
             Feed feed = new Feed();
             feed.setActivityId(rs.getLong("a.activity_id"));
-            feed.setDateTime(rs.getTimestamp("a.updated").toLocalDateTime());
+            feed.setDateTime(rs.getTimestamp("a.created").toLocalDateTime());
             feed.setTime(rs.getInt("a.time"));
             feed.setPosition(rs.getInt("a.position"));
             feed.setAthleteId(rs.getLong("b.id"));
