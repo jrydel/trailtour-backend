d4ff87d8c3dcb9dd96e55bddbacfd5fbda4b0166
jrydel <rydel@gloffer.com>
2020-05-04 01:07:02 +0200

    #U - sql fix

--- src/main/java/cz/jr/trailtour/backend/repository/ResultRepository.java
+++ src/main/java/cz/jr/trailtour/backend/repository/ResultRepository.java
@@ -24,7 +24,7 @@ public class ResultRepository extends MysqlRepository {
     }
 
     public List<Feed> getFeed(int limit) throws SQLException {
-        return selectList("SELECT a.activity_id, a.created, a.time, a.position, b.id, b.name, c.id, c.name, c.number, c.country FROM trailtour.result a JOIN trailtour.athlete b ON a.athlete_id = b.id JOIN trailtour.stage c ON c.id = a.stage_id ORDER BY a.updated DESC LIMIT ?", new Object[]{limit}, rs -> {
+        return selectList("SELECT a.activity_id, a.created, a.time, a.position, b.id, b.name, c.id, c.name, c.number, c.country FROM trailtour.result a JOIN trailtour.athlete b ON a.athlete_id = b.id JOIN trailtour.stage c ON c.id = a.stage_id ORDER BY a.created DESC LIMIT ?", new Object[]{limit}, rs -> {
             Feed feed = new Feed();
             feed.setActivityId(rs.getLong("a.activity_id"));
             feed.setDateTime(rs.getTimestamp("a.created").toLocalDateTime());
