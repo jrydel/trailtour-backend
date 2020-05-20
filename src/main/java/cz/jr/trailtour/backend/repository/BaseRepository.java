28f7c13ff8c9f6f6673fd1d2f2b81cd4f4e7dc5b
jrydel <rydel@gloffer.com>
2020-05-20 11:05:42 +0200

    #U - reworked queries

---
+++ src/main/java/cz/jr/trailtour/backend/repository/BaseRepository.java
@@ -0,0 +1,23 @@
+package cz.jr.trailtour.backend.repository;
+
+import com.zaxxer.hikari.HikariDataSource;
+import cz.jr.trailtour.backend.repository.mysql.MysqlRepository;
+import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+/**
+ * Created by Jiří Rýdel on 5/20/20, 10:56 AM
+ */
+@Repository
+public class BaseRepository extends MysqlRepository {
+
+    public BaseRepository(HikariDataSource dataSource) {
+        super(dataSource);
+    }
+
+    public LocalDateTime getLastResultUpdate(String database) throws SQLException {
+        return selectObject("SELECT MAX(timestamp) as max FROM " + database + ".athlete_result", new Object[]{}, rs -> rs.getTimestamp("max").toLocalDateTime());
+    }
+}
--- src/main/java/cz/jr/trailtour/backend/repository/StageRepository.java
+++ src/main/java/cz/jr/trailtour/backend/repository/StageRepository.java
@@ -3,26 +3,28 @@ package cz.jr.trailtour.backend.repository;
 import com.zaxxer.hikari.HikariDataSource;
 import cz.jr.trailtour.backend.repository.entities.stage.Stage;
 import cz.jr.trailtour.backend.repository.entities.stage.StageData;
-import cz.jr.trailtour.backend.repository.mysql.MysqlRepository;
 import cz.jr.trailtour.backend.repository.mysql.Param;
 import cz.jr.trailtour.backend.repository.mysql.UpsertParam;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * Created by Jiří Rýdel on 4/19/20, 1:21 PM
  */
 @Repository
-public class StageRepository extends MysqlRepository {
+public class StageRepository extends BaseRepository {
 
     public StageRepository(HikariDataSource dataSource) {
         super(dataSource);
     }
 
     public StageData get(String database, int number) throws SQLException {
-        return selectObject("SELECT a.name, a.type, a.distance, a.elevation, a.url, a.strava_url, a.strava_data, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number AND b.timestamp = (SELECT MAX(x.timestamp) FROM " + database + ".athlete_result x WHERE x.stage_number = a.number) WHERE a.number = ? GROUP by a.number", new Object[]{number}, rs -> {
+        LocalDateTime update = getLastResultUpdate(database);
+
+        return selectObject("SELECT a.name, a.type, a.distance, a.elevation, a.url, a.strava_url, a.strava_data, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number WHERE a.number = ? AND b.timestamp = ? GROUP by a.number", new Object[]{number, java.sql.Timestamp.valueOf(update)}, rs -> {
             StageData stage = new StageData();
             stage.setNumber(number);
             stage.setName(rs.getString("a.name"));
@@ -39,7 +41,9 @@ public class StageRepository extends MysqlRepository {
     }
 
     public List<Stage> getAll(String database) throws SQLException {
-        return selectList("SELECT a.number, a.name, a.url, a.distance, a.elevation, a.type, a.strava_url, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number AND b.timestamp = (SELECT MAX(x.timestamp) FROM " + database + ".athlete_result x WHERE x.stage_number = a.number) GROUP by a.number", new Object[]{}, rs -> {
+        LocalDateTime update = getLastResultUpdate(database);
+
+        return selectList("SELECT a.number, a.name, a.type,a.distance, a.elevation, a.url, a.strava_url, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number WHERE b.timestamp = ? GROUP by a.number", new Object[]{java.sql.Timestamp.valueOf(update)}, rs -> {
             Stage stage = new Stage();
             stage.setNumber(rs.getInt("a.number"));
             stage.setName(rs.getString("a.name"));
@@ -55,7 +59,9 @@ public class StageRepository extends MysqlRepository {
     }
 
     public List<StageData> getAllData(String database) throws SQLException {
-        return selectList("SELECT a.number, a.name, a.url, a.distance, a.elevation, a.type, a.strava_url, a.strava_data, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number AND b.timestamp = (SELECT MAX(x.timestamp) FROM " + database + ".athlete_result x WHERE x.stage_number = a.number) GROUP by a.number", new Object[]{}, rs -> {
+        LocalDateTime update = getLastResultUpdate(database);
+
+        return selectList("SELECT a.number, a.name, a.url, a.distance, a.elevation, a.type, a.strava_url, a.strava_data, a.mapycz_url, COUNT(*) AS activities FROM " + database + ".stage a LEFT JOIN " + database + ".athlete_result b ON a.number = b.stage_number WHERE b.timestamp = ? GROUP by a.number", new Object[]{java.sql.Timestamp.valueOf(update)}, rs -> {
             StageData stage = new StageData();
             stage.setNumber(rs.getInt("a.number"));
             stage.setName(rs.getString("a.name"));
